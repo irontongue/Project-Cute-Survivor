5cd7ab97f2839872aa1144d69aa54a9edfc0fbc3
irontongue <irontongue101@hotmail.com>
2024-04-28 15:24:05 +0930

    BeepBeep Fix + CarSprite Fix

--- Assets/BeepBeep.cs
+++ Assets/BeepBeep.cs
@@ -5,17 +5,9 @@ using UnityEngine;
 public class BeepBeep : MonoBehaviour
 {
     AudioSource audioSource;
-    [SerializeField] AudioClip clip;
-    [SerializeField, Range(0,1)] float volume = 1;
     private void OnEnable()
     {
-        AudioSource.PlayClipAtPoint(clip, transform.position, volume);
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        GetComponent<AudioSource>().Play();
         
     }
 }
